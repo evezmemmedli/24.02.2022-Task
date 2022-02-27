702e2c656777a32bb3709183ffabad5314b309f8
evezmemmedli <avazkm@code.edu.az>
2022-02-27 23:30:44 +0400

    Add project files.

---
+++ 24.02.2022 Task/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _24._02._2022_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ad ve soyad daxil edin");
+            string FullName = Console.ReadLine();
+            Console.WriteLine("Qrup daxil edin");
+            string GroupNo = Console.ReadLine();    
+            Console.WriteLine("Yas daxil edin");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Student student = new Student(FullName, GroupNo,num);
+
+        }
+    }
+}
